07e283ea1631133f83078d30e72ccdb55630bbef
Saikat Paul <saikat.paul@soroco.com>
2019-01-10 14:24:11 +0530

    bug-fixes

---
+++ src/app/shared-services/shared-services.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedServicesService {
+
+  KEY_CODE = {
+    RIGHT_ARROW: 39,
+    LEFT_ARROW: 37,
+    ESCAPE: 27,
+    ENTER: 13
+  }
+
+  constructor() { }
+}
