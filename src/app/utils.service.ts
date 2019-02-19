2da7faf64ee96f6acfaf2e7b4d47c72e32fc81b0
Anmol Dhingra <anmol.dhingra@soroco.com>
2019-02-19 18:19:09 +0530

    Adds util.service.ts

---
+++ src/app/utils.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilsService {
+
+  constructor() { }
+
+  /**
+   * Changes Date Object to String to send to Backend
+   * @param date: Date Object
+   */
+  datetypeToStringtype(date){
+      let myDate = new Date(date);
+      return myDate.getFullYear()+"-"+(myDate.getMonth()+1)+"-"+myDate.getDate()+" 00:00:00";
+  }
+  
+  formatDateToUS(date){
+    let myDate = new Date(date);
+    // console.log(myDate)
+    return (myDate.getMonth()+1)+"/"+myDate.getDate()+"/"+myDate.getFullYear();
+  }
+
+}
