459caf6d7247bc25358ce7aa71509568c3b6f9a4
Saikat Paul <saikat.paul@soroco.com>
2019-02-20 23:34:24 +0530

    removed contents in canvas-carousel component

--- src/app/components/createsop/canvas-crousel/canvas-crousel.component.ts
+++ src/app/components/createsop/canvas-crousel/canvas-crousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-canvas-crousel',
@@ -6,29 +6,10 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./canvas-crousel.component.scss']
 })
 export class CanvasCrouselComponent implements OnInit {
-  @ViewChild('canvasCarousel') canvasCarousel:ElementRef;
 
   constructor() { }
 
   ngOnInit() {
-    this.initialize()
   }
 
-  initialize(){
-    let canvas = this.canvasCarousel.nativeElement;
-    let canvasWidth = this.canvasCarousel.nativeElement.width;
-    let canvasHeight = this.canvasCarousel.nativeElement.height;
-    let ctx = canvas.getContext('2d');
-    
-    let img = new Image();
-    img.src = 'http://localhost:8000/sop/21/image/image1645125.531.jpeg';
-    img.width = canvasWidth;
-    img.height = canvasHeight;
-
-    img.onload = function(){
-      // ctx.imageSmoothingEnabled = false;
-      // ctx.filter = 'blur(0px)';
-      ctx.drawImage(img ,0, 0, img.width, img.height, 0, 0, img.width, img.height);
-    }
-  }
 }
