46e159d217dda480ada5d1f2b15ee393fd90da61
Saikat Paul <saikat.paul@soroco.com>
2019-02-27 04:17:11 +0000

    Step integration

--- src/app/components/createsop/left-panel/left-panel.component.ts
+++ src/app/components/createsop/left-panel/left-panel.component.ts
@@ -7,10 +7,34 @@ import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 })
 export class LeftPanelComponent implements OnInit {
 
+  currentImage :number=0;
+  imageGalleryContent = [];
   constructor() { }
 
   ngOnInit() {
 
   }
+
+    //for carousel
+    onCarouselMoveUp(){
+      /**
+       * this function is use to change the image in the carousel 
+       * by using the up button
+       */      
+      if(this.currentImage > 0){
+        this.currentImage = this.currentImage - 1;
+      }
+    }
+  
+    onCarouselMoveDown(){
+      /**
+       * this function is used to change the image in the carousel
+       * by using the down button
+       */
+
+      if(this.currentImage < this.imageGalleryContent.length - 1){
+        this.currentImage = this.currentImage + 1;
+      }
+    }
 
 }
--- src/app/components/createsop/steps-container/steps-container.component.ts
+++ src/app/components/createsop/steps-container/steps-container.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class StepsContainerComponent implements OnInit {
   @Input('stepType') stepType:string = 'Read';
+  @Input('stepIndex') stepIndex:number;
 
   constructor() { }
 
