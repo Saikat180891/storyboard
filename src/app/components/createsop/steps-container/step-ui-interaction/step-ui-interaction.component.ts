5a31870d32b9f7c500b37356ab0eb3dd7d19c19c
Saikat Paul <saikat.paul@soroco.com>
2019-02-28 13:57:26 +0530

    complete all ui data communication

--- src/app/components/createsop/steps-container/step-ui-interaction/step-ui-interaction.component.ts
+++ src/app/components/createsop/steps-container/step-ui-interaction/step-ui-interaction.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-step-ui-interaction',
@@ -8,19 +8,42 @@ import { Component, OnInit, Input } from '@angular/core';
 export class StepUiInteractionComponent implements OnInit {
   @Input('sectionId') sectionId:number;
   @Input('stepIndex') stepIndex:number;
-  canEdit:boolean = false;
+  @Input('sectionIndex') sectionIndex:number;
+  @Output('deleteStep') deleteStep = new EventEmitter();
+  @Output('outputChange') outputChange = new EventEmitter();
+  canEdit:boolean = true;
+  data = {
+    interaction_type:'',
+    click_option:'',
+    field:'',
+    notes:'',
+    exception_handling:'',
+    screen:'',
+    stepNumber:''
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.data.stepNumber = (this.sectionIndex + 1) + "." + (this.stepIndex + 1);
+    console.log(this.stepIndex, this.sectionIndex)
   }
 
   onClikedOnEdit(){
-    this.canEdit = false;
+    this.canEdit = !this.canEdit;
   }
 
   onClickOnOk(){
-    this.canEdit = true;
+    this.canEdit = false;
+    this.outputChange.emit({data:this.data, sectionIndex:this.sectionIndex, stepIndex:this.stepIndex, stepType: 'ui_interaction', sectionId: this.sectionId});
+  }
+
+  onCancelEdit(){
+    this.canEdit = false;
+  }
+
+  onDeleteStep(){
+    this.deleteStep.emit({sectionIndex:this.sectionIndex, stepIndex:this.stepIndex});
   }
 
 }
