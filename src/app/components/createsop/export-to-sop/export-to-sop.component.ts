81cc07cec96f8d8ac5741117bff21e1557e31f10
Saikat Paul <saikat.paul@soroco.com>
2019-03-07 14:08:41 +0530

    completed initial creation of screens

--- src/app/components/createsop/export-to-sop/export-to-sop.component.ts
+++ src/app/components/createsop/export-to-sop/export-to-sop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { DataService } from '../../../data.service';
 import { PageService } from '../services/page/page.service';
 import { ExportToSopService } from '../services/export-to-sop/export-to-sop.service';
@@ -20,9 +21,11 @@ export class ExportToSopComponent implements OnInit {
   screenName: string;
   tabName: string;
 
-  validateApplicationName: boolean = false;
-  validateScreenName: boolean = false;
-  validateTabName: boolean = false;
+  exportToSop = new FormGroup({
+    application_name: new FormControl('', Validators.required),
+    screen_name: new FormControl('', Validators.required),
+    tab_name: new FormControl('', Validators.required),
+  });
 
   /**
    *  required services are imported
@@ -42,54 +45,27 @@ export class ExportToSopComponent implements OnInit {
     this.close.emit(false);
   }
 
-  validateForm() {
-    let validationStatus = true;
-    if (this.applicationName === '') {
-      this.validateApplicationName = true;
-      validationStatus = false;
-    } else {
-      this.validateApplicationName = false;
-      validationStatus = true;
-    }
-    if (this.screenName === '') {
-      this.validateScreenName = true;
-      validationStatus = false;
-    } else {
-      this.validateScreenName = false;
-      validationStatus = true;
-    }
-    if (this.tabName === '') {
-      this.validateTabName = true;
-      validationStatus = false;
-    } else {
-      this.validateTabName = false;
-      validationStatus = true;
-    }
-    return validationStatus;
-  }
-
   /**
    * this function is called when the user clicks on the 'Export to SOP' button
    */
   onExportToSop() {
-    if (this.validateForm()) {
-      // the id of the image is fetched from the input payload
-      const { id } = this.data;
-      // the required payload for the api is created
-      const payload = {
-        image_id: id,
-        application_name: this.applicationName,
-        screen_name: this.screenName,
-        tab_name: this.tabName
-      };
-      // the call is executed at the below endpoint
-      const endpoint = `/sop/${this.__page.projectId}/epics/userstories/sections/screens.json`;
-      this.__api.post(endpoint, payload).subscribe(res => {
-        // response is pushed to the 'screenlist' array in the 'ExportToSopService'
-        this.__export.appendScreen(res);
-      });
-    } else {
-
-    }
+    // the id of the image is fetched from the input payload
+    const { id } = this.data;
+    // the required payload for the api is created
+    const payload = {
+      image_id: id,
+      ...this.exportToSop.value
+    };
+    // the call is executed at the below endpoint
+    const endpoint = `/sop/${this.__page.projectId}/epics/userstories/sections/screens.json`;
+    this.__api.post(endpoint, payload).subscribe(res => {
+      // response is pushed to the 'screenlist' array in the 'ExportToSopService'
+      this.__export.appendScreen(res);
+    }, err => {
+      this.onCloseExportModal();
+    },
+    () => {
+      this.onCloseExportModal();
+    });
   }
 }
