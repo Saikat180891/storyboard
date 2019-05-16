55e79c0025517f97534cc4828754732113f19d99
Manish Gautam <manish.gautam@soroco.com>
2019-05-16 02:29:29 -0400

    Merged PR 354: checking file format for images also
    checking file format for images also
    2739 - https://devops.soroco.com/Soroco%20Products/Storyboard/_workitems/edit/2739

--- src/app/components/createsop/video-gallery/video-gallery.component.ts
+++ src/app/components/createsop/video-gallery/video-gallery.component.ts
@@ -24,7 +24,8 @@ export class VideoGalleryComponent implements OnInit, OnChanges {
 
   selected: number = -1;
 
-  allowedFileFormats = ["video/mp4"];
+  allowedVideoFileFormats = ["video/mp4"];
+  allowedImageFileFormats = ["image/png", "image/jpeg"];
 
   constructor(
     private __sidebarService: SidebarService,
@@ -44,8 +45,18 @@ export class VideoGalleryComponent implements OnInit, OnChanges {
     this.optionSelected.emit({ index, content });
   }
 
-  onFileSelected($event: any) {
-    if (this.isAllowedFormatFile($event.target.files[0])) {
+  onImageFileSelected($event: any) {
+    if (this.isAllowedImageFormatFile($event.target.files[0])) {
+      this.addNewFile.emit($event.target.files[0]);
+    } else {
+      this.sharedService.raiseError(
+        "Only .png/.jpeg image file formats are allowed to upload"
+      );
+    }
+  }
+
+  onVideoFileSelected($event: any) {
+    if (this.isAllowedVideoFormatFile($event.target.files[0])) {
       this.addNewFile.emit($event.target.files[0]);
     } else {
       this.sharedService.raiseError(
@@ -62,7 +73,11 @@ export class VideoGalleryComponent implements OnInit, OnChanges {
     this.deleteContent.emit({ content, index: i, type });
   }
 
-  isAllowedFormatFile(file) {
-    return this.allowedFileFormats.includes(file.type);
+  isAllowedImageFormatFile(file) {
+    return this.allowedImageFileFormats.includes(file.type);
+  }
+
+  isAllowedVideoFormatFile(file) {
+    return this.allowedVideoFileFormats.includes(file.type);
   }
 }
